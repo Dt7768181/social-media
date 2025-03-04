47d12a9d7b3110b2b0b4ed538e65ba71873e9004
dt7768181 <dgt7768181@gmail.com>
2025-03-04 14:34:03 +0530

    added todo

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ function showHome(){
         <div class="p-2">
           <p class="text-info  p-2" onclick='Posts(${userid})'>Home</p>
           <p class="text-info  p-2" onclick='Album(${userid})'>Album</p>
-          <p class="text-info  p-2" onclick='Users(${userid})'>Profile</p>
           <p class="text-info  p-2" onclick='utodo(${userid})'>My TODO</p>
+          <p class="text-info  p-2" onclick='Users(${userid})'>Profile</p>
           <p class="text-danger  p-2" onclick='showLogin()'>Logout</p>
         </div>
         <div id='conten' class='p-2'></div>
@@ -62,7 +62,7 @@ function Posts(id){
   .then((data)=>displayPosts(data))
 }
 function displayPosts(data){
-  let str="";
+  let str="<h2>My posts</h2>";
   data.map((value)=>
   str+=`<div class='m-2'>
           <div class='p-2'><b><h4>Title:${value.title}</h4></b></div>
@@ -80,37 +80,37 @@ function Album(id) {
 }
 
 function displayAlbum(data) {
-  let str = "";
+  let str = "<h2>My album</h2>";
   data.forEach((value) => {
     str += `<div class='p-2'><b><h4>Title: ${value.title}</h4></b></div>`;
   });
   conten.innerHTML = str;
 }
 function Users(id) {
-  let str = "";
+  let str = "<h2>My profile<h2>";
   fetch(`https://jsonplaceholder.typicode.com/users?id=${id}`)
     .then((res) => res.json())
     .then((data) => {
         data.forEach((value) => {
           str += `<h4>${value.username}</h4><p>Name:${value.name}</p>
                   <p>Email: ${value.email}</p>
-                  <p>Address: ${value.address.street}, ${value.address.city}</p>`;
+                  <p>Address: ${value.address.suite} , ${value.address.street}, ${value.address.city}, ${value.address.zipcode}</p>`;
         });
-      conten.innerHTML = str; // Ensure there's an element with id="conten"
+      conten.innerHTML = str;
     })
     .catch((err) => console.error("Error fetching user:", err));
 }
 function utodo(id) {
-  let str = "";
+  let str = "<h2>TODOS</h2>";
   fetch(`https://jsonplaceholder.typicode.com/todos?userId=${id}`)
     .then((res) => res.json())
     .then((data) => {
         data.forEach((value) => {
-          str += `<h3>No.${value.id}</h3>
+          str += `
                   <h4>Title:${value.title}</h4>
-                  <h5>Completed ${value.completed}</h5>`;
+                  <input type="checkbox" ${value.completed && "checked"}>`;
         });
-      conten.innerHTML = str; // Ensure there's an element with id="conten"
+      conten.innerHTML = str; 
     })
     .catch((err) => console.error("Error fetching user:", err));
 }
